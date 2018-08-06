3e03a0301c916e6a3e4c6d2d1da758c4acd4b811
zimberzimber <tral2@walla.com>
2018-08-07 02:08:36 +0300

    -----   1.01 > Fixed broken SAIL interface by adding missing script (Which already exists in FU) > Fixed script inconsistency between ZBs and FUs research panel > Fixed research panel not registering currency as currency > Fixed research tree not auto-updating the view outside of low quality mode as it should > Fixed Quickbar issue (I think?) > Removed the Character Status option from the quickbar
    -----   File changes:
    	new file:   _previewimage
    	new file:   objects/scripts/customtechstation.lua
    
    	modified:   .metadata
    	modified:   _ZBversioning.config
    	modified:   quickbar/icons.json.patch
    	modified:   zb/researchTree/researchTree.lua

--- zb/researchTree/researchTree.lua
+++ zb/researchTree/researchTree.lua
@@ -1,7 +1,9 @@
 
 require("/zb/zb_util.lua")
+require("/scripts/util.lua")
 
 -- constants
+currencyTable = {}
 researchTree = nil
 gridTileSize = {}
 canvasSize = nil
@@ -29,6 +31,7 @@ function init()
 	-- zbutil.DeepPrintTable(status.statusProperty("zb_researchtree_researched", {}))
 	-- status.setStatusProperty("zb_researchtree_researched", nil)
 	
+	currencyTable = root.assetJson("/currencies.config")
 	data = root.assetJson("/zb/researchTree/data.config")
 	for _, file in ipairs(data.researchFiles) do
 		local temp = root.assetJson(file)
@@ -132,21 +135,21 @@ function update(dt)
 		end
 	end
 	
-	if mouseDown and not lowQuality then
-		local mousePos = canvas:mousePosition()
-		dragOffset.x = dragOffset.x + mousePos[1] - oldMousePos[1]
-		dragOffset.y = dragOffset.y + mousePos[2] - oldMousePos[2]
-		oldMousePos = mousePos
-		
-		-- idleRedrawCooldown = data.idleRedrawInterval
-		if not lowQuality then draw() end
-	-- else
-		-- if idleRedrawCooldown <= 0 then
-			-- idleRedrawCooldown = data.idleRedrawInterval
-			-- draw()
-		-- else
-			-- idleRedrawCooldown = idleRedrawCooldown - dt
-		-- end
+	if not lowQuality then
+		if mouseDown then
+			local mousePos = canvas:mousePosition()
+			dragOffset.x = dragOffset.x + mousePos[1] - oldMousePos[1]
+			dragOffset.y = dragOffset.y + mousePos[2] - oldMousePos[2]
+			oldMousePos = mousePos
+			
+			idleRedrawCooldown = data.idleRedrawInterval
+			draw()
+		elseif idleRedrawCooldown <= 0 then
+			idleRedrawCooldown = data.idleRedrawInterval
+			draw()
+		else
+			idleRedrawCooldown = idleRedrawCooldown - dt
+		end
 	end
 end
 
@@ -162,10 +165,8 @@ function researchButton()
 			player.giveBlueprint(researchTree[selected].unlocks)
 		end
 		
-		if researchTree[selected].func then
-			if _ENV[researchTree[selected].func] then
-				_ENV[researchTree[selected].func](researchTree[selected].params)
-			end
+		if researchTree[selected].func and _ENV[researchTree[selected].func] then
+			_ENV[researchTree[selected].func](researchTree[selected].params)
 		end
 		
 		local researchedTable = status.statusProperty("zb_researchtree_researched", {}) or {}
@@ -283,23 +284,28 @@ function updateInfoPanel()
 			end
 			
 			if researchTree[selected].price then
-				for i = 1, 9 do
-					local price = researchTree[selected].price[i]
-					if price then
-						local isCurrency = false
-						for _, tbl in ipairs(data.currencies) do
-							if price[1] == tbl[1] then
-								widget.setItemSlotItem("priceItem"..i, {name = tbl[3], count = price[2]})
-								isCurrency = true
-								break
-							end
-						end
-						
-						if not isCurrency then
-							widget.setItemSlotItem("priceItem"..i, {name = price[1], count = price[2]})
-						end
-					end
+				for i, tbl in ipairs(researchTree[selected].price) do
+					widget.setItemSlotItem("priceItem"..i, {name = tbl[1], count = tbl[2]})
+					if i >= 9 then break end
 				end
+				
+				
+				-- for i = 1, 9 do
+					-- local price = researchTree[selected].price[i]
+					-- if price then
+						-- local isCurrency = false
+						-- for _, tbl in ipairs(data.currencies) do
+							-- if price[1] == tbl[1] then
+								-- widget.setItemSlotItem("priceItem"..i, {name = tbl[3], count = price[2]})
+								-- isCurrency = true
+								-- break
+							-- end
+						-- end
+						
+						-- if not isCurrency then
+						-- end
+					-- end
+				-- end
 			end
 		else
 			widget.setText("title", data.strings.info[1])
@@ -743,14 +749,15 @@ end
 function buildStates(tree)
 	selected = nil
 	if tree then
-		researchTree = data.researchTree[tree]
 		selectedTree = tree
-	elseif not researchTree then
+	elseif not selectedTree then
 		return
 	end
 	
+	researchTree = copy(data.researchTree[selectedTree])
+	
 	local researchedTable = status.statusProperty("zb_researchtree_researched", {}) or {}
-	local dataString = researchedTable[tree] or ""
+	local dataString = researchedTable[selectedTree] or ""
 	local insertingString = ""
 	local splitString = {}
 	local splitpos = 0
@@ -767,16 +774,16 @@ function buildStates(tree)
 	
 	local isAvailable = true
 	for _, acr in ipairs(splitString) do
-		if data.acronyms[tree][acr] and researchTree[data.acronyms[tree][acr]] then
-			researchTree[data.acronyms[tree][acr]].state = "researched"
+		if data.acronyms[selectedTree][acr] and researchTree[data.acronyms[selectedTree][acr]] then
+			researchTree[data.acronyms[selectedTree][acr]].state = "researched"
 			
-			if researchTree[data.acronyms[tree][acr]].children then
-				for _, child in ipairs(researchTree[data.acronyms[tree][acr]].children) do
+			if researchTree[data.acronyms[selectedTree][acr]].children then
+				for _, child in ipairs(researchTree[data.acronyms[selectedTree][acr]].children) do
 					if researchTree[child].state ~= "researched" then
 						isAvailable = true
 						
 						for research, tbl in pairs(researchTree) do
-							if research ~= data.acronyms[tree][acr] and tbl.children then
+							if research ~= data.acronyms[selectedTree][acr] and tbl.children then
 								for _, child2 in ipairs(tbl.children) do
 									if child2 == child and tbl.state ~= "researched" then
 										isAvailable = false
@@ -797,10 +804,10 @@ function buildStates(tree)
 		end
 	end
 	
-	if data.hiddenResearch[tree] then
-		for _, acr in ipairs(data.hiddenResearch[tree]) do
-			if researchTree[data.acronyms[tree][acr]] and not researchTree[data.acronyms[tree][acr]].state then
-				hideResearchBranch(data.acronyms[tree][acr])
+	if data.hiddenResearch[selectedTree] then
+		for _, acr in ipairs(data.hiddenResearch[selectedTree]) do
+			if researchTree[data.acronyms[selectedTree][acr]] and not researchTree[data.acronyms[selectedTree][acr]].state then
+				hideResearchBranch(data.acronyms[selectedTree][acr])
 			end
 		end
 	end
@@ -823,16 +830,7 @@ function canAfford(research, consume)
 	
 	if researchTree[research].price then
 		for _, tbl in ipairs(researchTree[research].price) do
-			local isCurrency = false
-			
-			for _, curTbl in ipairs(data.currencies) do
-				if tbl[1] == curTbl[1] then
-					isCurrency = true
-					break
-				end
-			end
-			
-			if isCurrency then
+			if currencyTable[tbl[1]] then
 				if player.currency(tbl[1]) < tbl[2] then
 					return false
 				end
@@ -844,18 +842,10 @@ function canAfford(research, consume)
 		end
 	end
 	
+	-- Running the loops again so it consumes stuff AFTER checking that the player has everything
 	if consume then
 		for _, tbl in ipairs(researchTree[research].price) do
-			local isCurrency = false
-			
-			for _, curTbl in ipairs(data.currencies) do
-				if tbl[1] == curTbl[1] then
-					isCurrency = true
-					break
-				end
-			end
-			
-			if isCurrency then
+			if currencyTable[tbl[1]] then
 				player.consumeCurrency(tbl[1], tbl[2])
 			else
 				player.consumeItem({name = tbl[1], count = tbl[2]}, true)
@@ -998,4 +988,3 @@ function cheat(wd)
 		widget.setText(wd, "")
 	end
 end
-
